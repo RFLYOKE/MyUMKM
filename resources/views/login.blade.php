a56ea65664e8c67ece82ae0984c1e663395ea221
akmlrafly <akmalrafly875@gmail.com>
2025-05-12 19:31:56 +0700

    add: auth login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.auth')
+
+@section('title', 'Login | MyUMKM')
+
+@section('content')
+    <div class="flex items-center justify-center w-full h-screen">
+        <form action="#" method="POST">
+            @csrf
+            <div class="w-full border border-green-800 rounded-sm">
+                <input class=" pl-3 pr-10 py-3" type="text" placeholder="Nama Pengguna / Email" required>
+            </div>
+            <div class="border border-green-800 rounded-sm mt-6">
+                <input class="pl-3 pr-10 py-3" type="text" placeholder="Nama Pengguna / Email" required>
+            </div>
+            <div class="flex justify-end">
+                <button class="text-[#4A7744] text-sm">bantuan</button>
+            </div>
+            <div class="flex justify-center mt-4">
+                <button type="submit"
+                    class="w-3/5 py-2 text-sm bg-[#40693B] text-white font-semibold rounded-md hover:bg-green-700 transition">
+                    Lanjut
+                </button>
+            </div>
+            <p class="text-center text-sm text-green-800 mt-2">
+                Belum punya akun? <a href="#" class="font-semibold hover:underline">Daftar</a>
+            </p>
+        </form>
+    </div>
+@endsection
