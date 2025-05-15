a300362dccfbf2149b6c146e0d3a5d1a50a93712
akmlrafly <akmalrafly875@gmail.com>
2025-05-15 20:57:51 +0700

    update:corousel

--- resources/views/components/corousel.blade.php
+++ resources/views/components/corousel.blade.php
@@ -1,7 +1,18 @@
-<div>
-    <div class="h-24 bg-[#4A7744] relative p-1">
-        <div>
-            <img src="{{ asset('icon/itemCorousel.png') }}" alt="" class="absolute left-2 top-0">
+<div class="h-32 bg-[#4A7744] relative px-2 flex items-center">
+    <div>
+        <img src="{{ asset('icon/itemCorousel.png') }}" alt="">
+        <div class="absolute top-4 left-3">
+            <p class="font-semibold text-white w-32 text-center">Promo akhir tahun</p>
+            <p class="font-bold text-white w-32 text-center text-3xl">12.12</p>
         </div>
+    </div>
+    <div class="p-2">
+        <p class="text-center text-white text-sm font-semibold">Belanja Apapun Jadi Lebih Murah</p>
+        <div class="flex text-white justify-between items-center my-2 gap-x-3">
+            <span class="w-1/4 text-sm bg-orange-500 p-1 rounded-lg">Diskon Hingga</span>
+            <p class="w-3/4 text-2xl font-semibold"><span class="text-sm">Rp</span>100 <span class="text-sm">rb*</span></p>
+        </div>
+        <hr>
+        <p class="text-white font-semibold text-sm">Bebas Ongkir Rp <span class="text-2xl text-orange-500">0</span></p>
     </div>
 </div>
